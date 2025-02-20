2f1c0a05f902f79733222cbc7dd954f406298f94
yadavpramod28 <yadavpramod28@gmail.com>
2025-02-20 22:34:29 +0530

    creating my first java project

---
+++ simple-web-app/build/webapp/index.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple Web Application</title>
+</head>
+<body>
+    <h1>Welcome to the Simple Web Application!</h1>
+    <a href="hello">Click here to see the HelloServlet</a>
+</body>
+</html>
+
---
+++ simple-web-app/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
+}
+
